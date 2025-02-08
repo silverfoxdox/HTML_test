1fe9fea5cf13f6875f7cf4952a3ea0aa961e4fdf
Tim Pangborn <t1mpangb0rn@gmail.com>
2025-02-07 17:43:21 -0800

    Fixed imports and function

--- PythonHTMLText.py
+++ PythonHTMLText.py
@@ -1,20 +1,24 @@
+from bs4 import BeautifulSoup
 
 
-def html_to_text(html_file, text_file):
+def html_to_text():
     # Open and read the HTML file with r at the beginning indicates a raw string
     with open(r'C:\Users\t1mpa\OneDrive\Documents\Text\sample-html-files-sample2.html', 'r', encoding='utf-8') as file:
         html_content = file.read()
-
+    print(html_content)
     # Parse the HTML content
     soup = BeautifulSoup(html_content, 'html.parser')
 
     # Extract text from the parsed HTML
     text_content = soup.get_text()
+    print(text_content)
 
     # Write the text content to a text file
-    with open(r'C:\Users\t1mpa\OneDrive\Documents\Text\H to T.txt', 'w', encoding='utf-8') as file:
+    with open(r'C:\Users\t1mpa\OneDrive\Documents\Text\htotext.txt', 'w', encoding='utf-8') as file:
         file.write(text_content)
 
+
+html_to_text()
 
 # Example usage
 # html_file = 'example.html'
